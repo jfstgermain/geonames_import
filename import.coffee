ad8720b43ac02705df8c41a3b164314e22945e75
Jean-François St-Germain <jfstgermain@gmail.com>
2011-12-13 21:08:57 -0500

    imports all canadian cities and links provinces to them.  need to support districts

--- import.coffee
+++ import.coffee
@@ -2,42 +2,42 @@ csv = require "ya-csv"
 mongodb = require "mongodb"
 db = new mongodb.Db("geonames", new mongodb.Server "127.0.0.1", 27017, {})
 # include only cities, boroughs & districts
-interesting_feature_codes = ["PPL", "PPLA", "PPLC", "PPLG", "PPLL", "PPLR", "PPLS", "STLMT", "PPLQ", "PPLW"]
+interesting_feature_codes = ["PPL", "PPLA", "PPLC", "PPLL", "PPLS"]
 interesting_country_codes = ["CA", "US", "FR"]
-counter = 0
 
 db.open (err, db) ->
   # Add US & CAN states / provinces
   db.collection "states_dump", (err, states_dump) ->
     states_dump.createIndex [['state_code', 1]], (indexErr, indexName) ->
       if not indexErr?
+        counter = 0
         reader = csv.createCsvFileReader "./data/admin1CodesASCII.csv", {separator: "\t"}
         reader.addListener "data", (data) ->
-          [state_code, state_name1, state_name2, geonameid] = data      
+          [state_code, state_name1, state_name2, geonameid] = data
           [state_code_prefix, state_code_suffix] = state_code.split(".")
         
           if state_code_prefix in interesting_country_codes
-            doc = 
+            doc =
               geonameid: geonameid
               state_code: state_code
               state_name1: state_name1
               state_name2: state_name2
           
-          states_dump.insert doc, (doc) ->
-            counter += 1
-            console.log "#{counter}" if counter % 1000 == 0
+            states_dump.insert doc, (doc2) ->
+              counter++
+              console.log "#{counter}" if counter % 1000 is 0
             
         reader.addListener "end", ->
           # if state_code 
           # Import cities with population > 1000
           # Need to include districts also not just cities
           db.collection "countries_dump", (err1, countries_dump) ->
-            reader = csv.createCsvFileReader "./data/cities.csv", {separator: "\t"}
+            reader = csv.createCsvFileReader "./data/CA.csv", {separator: "\t"}
             reader.addListener "data", (data) ->
               [geonameid, name, asciiname, alternatenames, latitude, longitude, feature_class, feature_code, country_code, cc2, admin1_code, admin2_code, admin3_code, admin4_code, population, elevation, gtopo30, timezone, modification_date] = data
         
               #if feature_code in interesting_feature_codes and "(historical)" not in name
-              if country_code in interesting_country_codes and "(historical)" not in name
+              if feature_code in interesting_feature_codes and "(historical)" not in name
                 # add the statename to the keywords array field
                 ###
                 statename = (not admin1_code.nil? and not states[admin1_code].nil?) ? states[admin1_code].downcase : nil
@@ -46,18 +46,21 @@ db.open (err, db) ->
                 keywords.delete_if {|x| x == ""}
                 keywords = keywords.flatten.uniq.sort
                 ###
+                admin1_code_full = "#{country_code}.#{admin1_code}"
                 # set up the hash to be inserted into MongoDB
-                states_dump.find {state_code: admin1_code}, (err2, cursor) ->
+                states_dump.find {state_code: admin1_code_full}, (err2, cursor) ->
                   cursor.nextObject (err3, admin1_code_doc) ->
                     admin1_code_dbref = null
+                    counter = 0
+
                     if not err? and admin1_code_doc?
-                      admin1_code_dbref = new client.bson_serializer.DBRef "states_dump", admin1_code_doc._id, "geonames"
-                    else console.log "Couldn't find #{admin1_code} in  states_dump"
+                      admin1_code_dbref = new db.bson_serializer.DBRef "states_dump", admin1_code_doc._id, "geonames"
+                    else console.log "Couldn't find #{admin1_code_full} in states_dump"
                       
-                    doc = 
+                    doc =
                       geonameid: geonameid
                       name: name
-                      loc: 
+                      loc:
                         lat: (Number) latitude
                         lng: (Number) longitude
                       feature_class: feature_class
@@ -75,5 +78,9 @@ db.open (err, db) ->
                       timezone: timezone
                       
                     countries_dump.insert doc, (doc) ->
-                      counter += 1
+                      counter++
                       console.log "#{counter}" if counter % 1000 == 0
+
+            reader.addListener "end", ->
+              console.log "Finished importing records"
+
--- import.js
+++ import.js
@@ -1,5 +1,5 @@
 (function() {
-  var counter, csv, db, interesting_feature_codes, mongodb;
+  var csv, db, interesting_country_codes, interesting_feature_codes, mongodb;
   var __indexOf = Array.prototype.indexOf || function(item) {
     for (var i = 0, l = this.length; i < l; i++) {
       if (this[i] === item) return i;
@@ -8,53 +8,102 @@
   };
   csv = require("ya-csv");
   mongodb = require("mongodb");
-  db = new mongodb.Db("meatme", new mongodb.Server("127.0.0.1", 27017, {}));
-  interesting_feature_codes = ["PPL", "PPLA", "PPLC", "PPLG", "PPLL", "PPLR", "PPLS", "STLMT", "PPLQ", "PPLW"];
-  counter = 0;
+  db = new mongodb.Db("geonames", new mongodb.Server("127.0.0.1", 27017, {}));
+  interesting_feature_codes = ["PPL", "PPLA", "PPLC", "PPLL", "PPLS"];
+  interesting_country_codes = ["CA", "US", "FR"];
   db.open(function(err, db) {
-    return db.collection("test", function(err, collection) {
-      var reader;
-      reader = csv.createCsvFileReader("./data/CA.csv", {
-        separator: "\t"
-      });
-      return reader.addListener("data", function(data) {
-        var admin1_code, admin2_code, admin3_code, admin4_code, alternatenames, asciiname, cc2, country_code, doc, elevation, feature_class, feature_code, geonameid, gtopo30, latitude, longitude, modification_date, name, population, timezone;
-        geonameid = data[0], name = data[1], asciiname = data[2], alternatenames = data[3], latitude = data[4], longitude = data[5], feature_class = data[6], feature_code = data[7], country_code = data[8], cc2 = data[9], admin1_code = data[10], admin2_code = data[11], admin3_code = data[12], admin4_code = data[13], population = data[14], elevation = data[15], gtopo30 = data[16], timezone = data[17], modification_date = data[18];
-        if (__indexOf.call(interesting_feature_codes, feature_code) >= 0 && __indexOf.call(name, "(historical)") < 0) {
-          /*
-                  statename = (not admin1_code.nil? and not states[admin1_code].nil?) ? states[admin1_code].downcase : nil
-                  alternatnames.gsub!(/\./,"")
-                  keywords = [name.downcase.split(" "),alternatenames.downcase.split(",").split(" "),admin1_code.downcase,admin2_code.downcase,admin3_code.downcase,admin4_code.downcase,statename].flatten.compact
-                  keywords.delete_if {|x| x == ""}
-                  keywords = keywords.flatten.uniq.sort
-                  */
-          doc = {
-            geonameid: geonameid,
-            name: name,
-            asciiname: asciiname,
-            alternatenames: alternatenames,
-            loc: {
-              lat: Number(latitude),
-              lng: Number(longitude)
-            },
-            feature_class: feature_class,
-            feature_code: feature_code,
-            country_code: country_code,
-            cc2: cc2,
-            admin1_code: admin1_code,
-            admin2_code: admin2_code,
-            admin3_code: admin3_code,
-            admin4_code: admin4_code,
-            population: population,
-            elevation: elevation,
-            gtopo30: gtopo30,
-            timezone: timezone
-          };
-          return collection.insert(doc, function(doc) {
-            counter = counter + 1;
-            if (counter % 100000 === 0) {
-              return console.log("" + counter);
+    return db.collection("states_dump", function(err, states_dump) {
+      return states_dump.createIndex([['state_code', 1]], function(indexErr, indexName) {
+        var counter, reader;
+        if (!(indexErr != null)) {
+          counter = 0;
+          reader = csv.createCsvFileReader("./data/admin1CodesASCII.csv", {
+            separator: "\t"
+          });
+          reader.addListener("data", function(data) {
+            var doc, geonameid, state_code, state_code_prefix, state_code_suffix, state_name1, state_name2, _ref;
+            state_code = data[0], state_name1 = data[1], state_name2 = data[2], geonameid = data[3];
+            _ref = state_code.split("."), state_code_prefix = _ref[0], state_code_suffix = _ref[1];
+            if (__indexOf.call(interesting_country_codes, state_code_prefix) >= 0) {
+              doc = {
+                geonameid: geonameid,
+                state_code: state_code,
+                state_name1: state_name1,
+                state_name2: state_name2
+              };
+              return states_dump.insert(doc, function(doc2) {
+                counter++;
+                if (counter % 1000 === 0) {
+                  return console.log("" + counter);
+                }
+              });
             }
+          });
+          return reader.addListener("end", function() {
+            return db.collection("countries_dump", function(err1, countries_dump) {
+              reader = csv.createCsvFileReader("./data/CA.csv", {
+                separator: "\t"
+              });
+              reader.addListener("data", function(data) {
+                var admin1_code, admin1_code_full, admin2_code, admin3_code, admin4_code, alternatenames, asciiname, cc2, country_code, elevation, feature_class, feature_code, geonameid, gtopo30, latitude, longitude, modification_date, name, population, timezone;
+                geonameid = data[0], name = data[1], asciiname = data[2], alternatenames = data[3], latitude = data[4], longitude = data[5], feature_class = data[6], feature_code = data[7], country_code = data[8], cc2 = data[9], admin1_code = data[10], admin2_code = data[11], admin3_code = data[12], admin4_code = data[13], population = data[14], elevation = data[15], gtopo30 = data[16], timezone = data[17], modification_date = data[18];
+                if (__indexOf.call(interesting_feature_codes, feature_code) >= 0 && __indexOf.call(name, "(historical)") < 0) {
+                  /*
+                                  statename = (not admin1_code.nil? and not states[admin1_code].nil?) ? states[admin1_code].downcase : nil
+                                  alternatnames.gsub!(/\./,"")
+                                  keywords = [name.downcase.split(" "),alternatenames.downcase.split(",").split(" "),admin1_code.downcase,admin2_code.downcase,admin3_code.downcase,admin4_code.downcase,statename].flatten.compact
+                                  keywords.delete_if {|x| x == ""}
+                                  keywords = keywords.flatten.uniq.sort
+                                  */
+                  admin1_code_full = "" + country_code + "." + admin1_code;
+                  return states_dump.find({
+                    state_code: admin1_code_full
+                  }, function(err2, cursor) {
+                    return cursor.nextObject(function(err3, admin1_code_doc) {
+                      var admin1_code_dbref, doc;
+                      admin1_code_dbref = null;
+                      counter = 0;
+                      if (!(err != null) && (admin1_code_doc != null)) {
+                        console.dir(admin1_code_doc);
+                        admin1_code_dbref = new db.bson_serializer.DBRef("states_dump", admin1_code_doc._id, "geonames");
+                      } else {
+                        console.log("Couldn't find " + admin1_code_full + " in states_dump");
+                      }
+                      doc = {
+                        geonameid: geonameid,
+                        name: name,
+                        loc: {
+                          lat: Number(latitude),
+                          lng: Number(longitude)
+                        },
+                        feature_class: feature_class,
+                        feature_code: feature_code,
+                        country_code: country_code,
+                        cc2: cc2,
+                        admin1_code: admin1_code,
+                        admin1_code_dbref: admin1_code_dbref,
+                        admin2_code: admin2_code,
+                        admin3_code: admin3_code,
+                        admin4_code: admin4_code,
+                        population: population,
+                        elevation: elevation,
+                        gtopo30: gtopo30,
+                        timezone: timezone
+                      };
+                      return countries_dump.insert(doc, function(doc) {
+                        counter++;
+                        if (counter % 1000 === 0) {
+                          return console.log("" + counter);
+                        }
+                      });
+                    });
+                  });
+                }
+              });
+              return reader.addListener("end", function() {
+                return console.log("Finished importing records");
+              });
+            });
           });
         }
       });
